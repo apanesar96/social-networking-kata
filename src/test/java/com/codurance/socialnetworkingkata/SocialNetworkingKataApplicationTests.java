4d7adbde9c1dfadf348aaf0127896806d5507b32
Aman Panesar <amanpanesar@Amans-MacBook-Pro.local>
2021-09-23 10:25:06 +0100

    initial commit

---
+++ src/test/java/com/codurance/socialnetworkingkata/SocialNetworkingKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codurance.socialnetworkingkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkingKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
