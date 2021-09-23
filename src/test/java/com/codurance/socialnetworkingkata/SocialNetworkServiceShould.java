cc9aa00bae44bbd7e40af05c28e9884d48a51827
jordancodurance <jordan.colgan@codurance.com>
2021-09-23 16:13:25 +0100

    Clean up social network service test

--- src/test/java/com/codurance/socialnetworkingkata/SocialNetworkServiceShould.java
+++ src/test/java/com/codurance/socialnetworkingkata/SocialNetworkServiceShould.java
@@ -8,70 +8,55 @@ import com.codurance.socialnetworkingkata.user.Post;
 import com.codurance.socialnetworkingkata.user.User;
 import com.codurance.socialnetworkingkata.user.User.UserBuilder;
 import com.codurance.socialnetworkingkata.user.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InOrder;
 
 import java.time.Instant;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-@ExtendWith(MockitoExtension.class)
 class SocialNetworkServiceShould {
 
-	@Mock
-	private Console console;
+    private final Console console = mock(Console.class);
+    private final InputableCommandMapper inputableCommandMapper = new InputableCommandMapper();
+    private final UserRepository userRepository = mock(UserRepository.class);
+    private final DurationDeterminer durationDeterminer = mock(DurationDeterminer.class);
+    private final ExecutableCommandFactory executableCommandFactory = new ExecutableCommandFactory(userRepository, console, durationDeterminer);
 
-	private InputableCommandMapper inputableCommandMapper;
+    private final SocialNetworkService socialNetworkService = new SocialNetworkService(console, inputableCommandMapper, executableCommandFactory);
 
-	private ExecutableCommandFactory executableCommandFactory;
+    @Test
+    void update_user_timeline_with_message_on_submission_of_post_command() {
+        given(console.readInput()).willReturn("posting: Alice -> I love the weather today");
+        User alice = new UserBuilder().createUser();
+        given(userRepository.get("Alice")).willReturn(alice);
 
-	@Mock
-	private UserRepository userRepository;
+        socialNetworkService.submit();
 
-	@Mock
-	private DurationDeterminer durationDeterminer;
+        verify(userRepository).updateTimeline(alice, "I love the weather today");
+    }
 
-	private SocialNetworkService socialNetworkService;
+    @Test
+    void output_user_timeline_on_submission_of_read_command() {
+        Instant postTimestamp = Instant.now();
+        List<Post> timeline = List.of(
+                new Post("Good game though.", postTimestamp),
+                new Post("Damn! We lost!", postTimestamp)
+        );
+        User bob = new UserBuilder().withTimeline(timeline).createUser();
+        given(console.readInput()).willReturn("reading: Bob");
+        given(userRepository.get("Bob")).willReturn(bob);
+        given(durationDeterminer.calculateDurationFromNow(postTimestamp)).willReturn("1 minute ago", "2 minutes ago");
 
+        socialNetworkService.submit();
 
-	@BeforeEach
-	void setUp() {
-		inputableCommandMapper = new InputableCommandMapper();
-		executableCommandFactory = new ExecutableCommandFactory(userRepository, console, durationDeterminer);
-		socialNetworkService = new SocialNetworkService(console, inputableCommandMapper, executableCommandFactory);
-	}
+        InOrder inOrder = inOrder(console);
+        inOrder.verify(console).output("Good game though. (1 minute ago)");
+        inOrder.verify(console).output("Damn! We lost! (2 minutes ago)");
+    }
 
-	@Test
-	void update_user_timeline_with_message_on_submission_of_post_command() {
-		given(console.readInput()).willReturn("posting: Alice -> I love the weather today");
-		User alice = new UserBuilder().createUser();
-		given(userRepository.get("Alice")).willReturn(alice);
-
-		socialNetworkService.submit();
-
-		verify(userRepository).updateTimeline(alice, "I love the weather today");
-	}
-
-	@Test
-	void output_user_timeline_on_submission_of_read_command() {
-		Instant postTimestamp = Instant.now();
-		List<Post> timeline = List.of(
-				new Post("Good game though.", postTimestamp),
-				new Post("Damn! We lost!", postTimestamp)
-		);
-		User bob = new UserBuilder().withTimeline(timeline).createUser();
-		given(console.readInput()).willReturn("reading: Bob");
-		given(userRepository.get("Bob")).willReturn(bob);
-		given(durationDeterminer.calculateDurationFromNow(postTimestamp)).willReturn("1 minute ago", "2 minutes ago");
-
-		socialNetworkService.submit();
-
-		verify(console).output("Good game though. (1 minute ago)");
-		verify(console).output("Damn! We lost! (2 minutes ago)");
-	}
 }
